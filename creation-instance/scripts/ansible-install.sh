03572af3bf7ec9c9ca70a26a805d5c5336dfea1d
srinivasmunugoti <srinivasmunugoti2204@gmail.com>
2024-01-08 14:38:26 +0530

    second

--- creation-instance/scripts/ansible-install.sh
+++ creation-instance/scripts/ansible-install.sh
@@ -1,8 +1,23 @@
 sudo apt-add-repository ppa:ansible/ansible
 sudo apt update
 sudo apt install ansible -y
-sudo ansible-galaxy install nginxinc.nginx -y
-ansible-playbook playbook.yml
+
+
+cat <<EOF >> playbook.yml
+---
+- hosts: localhost
+  tasks:
+    - name: install nginx
+      apt: name=nginx state=latest
+    - name: start nginx
+      service:
+          name: nginx
+          state: started
+EOF
+ 
+sudo chmod +x playbook.yml
+sudo ansible-playbook playbook.yml
+
 
 
 
